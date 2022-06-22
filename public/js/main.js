30a017aa22ee3bdbef5f620b564d0b6161ce142d
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-06-22 18:08:47 +0200

    added refresh button

--- public/js/main.js
+++ public/js/main.js
@@ -23,4 +23,22 @@ update.addEventListener('click', _ => {
     })
   })
 
+  function refreshPage(){
+    window.location.reload();
+    } 
+    
+    function toggleCssMenu(icon) {
+        var cssmenu = document.getElementById('cssmenu');
+        if (icon.className.indexOf('active') == -1) {
+            icon.className = 'menu-icon active';
+            cssmenu.style.display = "block";
+            setTimeout(function(){cssmenu.className = 'active';},0);            
+        }
+        else {
+            icon.className = 'menu-icon';
+            cssmenu.className = '';
+            setTimeout(function(){cssmenu.style.display = "none";},411); 
+        }
+    }
+
 
